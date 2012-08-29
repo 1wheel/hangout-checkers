8682cf264c83df4b8f06b12e0c2c67e1ab1fab71
Adam Pearce <1wheel@gmail.com>
2012-08-28 19:32:55 -0500

    removed

--- reversi/game.js
+++ reversi/game.js
@@ -336,7 +336,7 @@ gapi.hangout.onApiReady.add(function(eventObj){
 		var state = gapi.hangout.data.getState();
 		
 		//checks to see if game has already been created
-		if (typeof state.cArray === 'undefined') {
+		if (typeof state.cArray == 'undefined') {
 			//game already running, join it
 			setupCanvasObjects();
 			serverUpdate();
